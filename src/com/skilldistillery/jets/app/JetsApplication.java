eafc76c411b2cedccfff073375215c20e0dfb12a
Andy Cary <info@andycary.com>
2022-03-07 08:58:03 -0500

    populate AirField and use try-with-resources on Scanner

--- src/com/skilldistillery/jets/app/JetsApplication.java
+++ src/com/skilldistillery/jets/app/JetsApplication.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import com.skilldistillery.jets.entities.AirField;
 import com.skilldistillery.jets.entities.Cargo;
 import com.skilldistillery.jets.entities.CargoCarrier;
 import com.skilldistillery.jets.entities.Jet;
@@ -15,191 +16,192 @@ import com.skilldistillery.jets.entities.JetImpl;
 public class JetsApplication {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in).useDelimiter("\n");
+		try (Scanner sc = new Scanner(System.in).useDelimiter("\n")) {
+			// Create aircraft and populate airfield
+			JetsApplication app = new JetsApplication();
+			// Jets
+			List<Jet> jets = new ArrayList<>();
+			jets = app.readJetFromFile("jets.txt");
+			// Cargo
+			List<Cargo> cargo = new ArrayList<>();
+			cargo = app.readCargoFromFile("cargo.txt");
 
-		// Create aircraft and populate airfield
-		JetsApplication app = new JetsApplication();
-		// Jets
-		List<Jet> jets = new ArrayList<>();
-		jets = app.readJetFromFile("jets.txt");
-		// Cargo
-		List<Cargo> cargo = new ArrayList<>();
-		cargo = app.readCargoFromFile("cargo.txt");
+			// Populate airfield
+			System.out.println("Populating airfield...READY!\n");
+			AirField airField = new AirField();
+			airField.airCommand.addAll(jets);
+			airField.airCommand.addAll(cargo);
 
-		// Populate airfield
-		System.out.println("Populating airfield...READY!\n");
-		List<Jet> airCommand = jets;
-		airCommand.addAll(cargo);
+			// Get command
+			System.out.println("**** GREETINGS, COMMANDER! ****\n");
+			app.greet();
+			String selection = app.getSelection(sc);
 
-		// Get command
-		System.out.println("**** GREETINGS, COMMANDER! ****\n");
-		app.greet();
-		String selection = app.getSelection(sc);
-
-		// Interact for user input
-		while (true) {
-			switch (selection) {
-			case "1":
-				System.out.println();
-				System.out.println("1. List fleet:\n");
-				for (Jet jet : airCommand) {
-					System.out.println("* " + jet);
-				}
-				System.out.println();
-				break;
-			case "2":
-				System.out.println("\n2. Fly all jets:\n");
-				for (Jet jet : airCommand) {
-					jet.fly();
-				}
-				System.out.println();
-				break;
-			case "3":
-				System.out.println();
-				System.out.println("3. View fastest jet:\n");
-				Jet fastestJet = null;
-				try {
-					fastestJet = airCommand.get(0);
-				} catch (Exception e) {
-					System.out.println("An error occurred.\n");
-					break;
-				}
-				if (fastestJet != null) {
-					for (int i = 1; i < airCommand.size(); i++) {
-						if (airCommand.get(i).getSpeedMph() > fastestJet.getSpeedMph()) {
-							fastestJet = airCommand.get(i);
-						}
-					}
-					System.out.println(
-							"* Fastest jet: " + fastestJet.getModel() + " @ " + fastestJet.getSpeedMph() + " MPH");
-				}
-				System.out.println();
-				break;
-			case "4":
-				System.out.println("\n4. View jet with longest range:\n");
-				Jet farthestJet = null;
-				try {
-					farthestJet = airCommand.get(0);
-				} catch (Exception e) {
-					System.out.println("An error occurred.\n");
-					break;
-				}
-				if (airCommand != null) {
-					for (int i = 1; i < airCommand.size(); i++) {
-						if (airCommand.get(i).getRange() > farthestJet.getRange()) {
-							farthestJet = airCommand.get(i);
-						}
-					}
-					System.out.println("* Longest range jet: " + farthestJet.getModel() + " @ " + farthestJet.getRange()
-							+ " miles");
-				}
-				System.out.println();
-				break;
-			case "5":
-				System.out.println("\n5. Load all Cargo Jets:\n");
-				for (Jet cargoCarrier : airCommand) {
-					if (cargoCarrier instanceof CargoCarrier) {
-						((CargoCarrier) cargoCarrier).loadCargo();
-					}
-				}
-				System.out.println();
-				break;
-			case "6":
-				System.out.println("\n6. Dogfight!\n");
-				for (Jet jet : airCommand) {
-					if (jet instanceof JetImpl) {
-						jet.fight();
-					}
-				}
-				System.out.println();
-				break;
-			case "7":
-				System.out.println();
-				System.out.println("7. Add a jet to Fleet:\n");
-				String name;
-				int speed;
-				long range;
-				double price;
-				System.out.println("****************");
-				System.out.println("Enter jet Model:");
-				try {
-					name = sc.next();
-				} catch (Exception e) {
-					System.out.println("An error occurred.\n");
-					break;
-				}
-				System.out.println("Enter jet Speed:");
-				try {
-					speed = sc.nextInt();
-				} catch (Exception e) {
-					System.out.println("An error occurred.\n");
-					break;
-				}
-				System.out.println("Enter jet Range:");
-				try {
-					range = sc.nextLong();
-				} catch (Exception e) {
-					System.out.println("An error occurred.\n");
-					break;
-				}
-				System.out.println("Enter jet Price:");
-				try {
-					price = sc.nextDouble();
-				} catch (Exception e) {
-					System.out.println("An error occurred.\n");
-					break;
-				}
-				System.out.println();
-				Jet newJet = new JetImpl(name, speed, range, price);
-				airCommand.add(newJet);
-				System.out.println("Added!");
-				for (Jet jet : airCommand) {
-					System.out.println("* " + jet);
-				}
-				System.out.println();
-				break;
-			case "8":
-				System.out.println("\n8. Remove a jet from Fleet\n");
-
-				System.out.println("Available jets:\n");
-
-				for (int i = 0; i < airCommand.size(); i++) {
-					System.out.println(i + ": " + airCommand.get(i));
-				}
-
-				int toRemove;
-
-				System.out.println("\nWhich jet to remove?\n");
-				try {
-					toRemove = sc.nextInt();
-				} catch (Exception e) {
-					System.out.println("\nUh oh, something happened.\n");
-					System.out.println("\nWhich jet to remove?\n");
-					toRemove = sc.nextInt();
-				}
-
-				try {
-					airCommand.remove(toRemove);
-					System.out.println("Removed.\n");
-					for (Jet jet : airCommand) {
+			// Interact for user input
+			while (true) {
+				switch (selection) {
+				case "1":
+					System.out.println();
+					System.out.println("1. List fleet:\n");
+					for (Jet jet : airField.airCommand) {
 						System.out.println("* " + jet);
 					}
-				} catch (Exception e) {
-					System.out.println("Hmmm, please try again.");
-				}
+					System.out.println();
+					break;
+				case "2":
+					System.out.println("\n2. Fly all jets:\n");
+					for (Jet jet : airField.airCommand) {
+						jet.fly();
+					}
+					System.out.println();
+					break;
+				case "3":
+					System.out.println();
+					System.out.println("3. View fastest jet:\n");
+					Jet fastestJet = null;
+					try {
+						fastestJet = airField.airCommand.get(0);
+					} catch (Exception e) {
+						System.out.println("An error occurred.\n");
+						break;
+					}
+					if (fastestJet != null) {
+						for (int i = 1; i < airField.airCommand.size(); i++) {
+							if (airField.airCommand.get(i).getSpeedMph() > fastestJet.getSpeedMph()) {
+								fastestJet = airField.airCommand.get(i);
+							}
+						}
+						System.out.println(
+								"* Fastest jet: " + fastestJet.getModel() + " @ " + fastestJet.getSpeedMph() + " MPH");
+					}
+					System.out.println();
+					break;
+				case "4":
+					System.out.println("\n4. View jet with longest range:\n");
+					Jet farthestJet = null;
+					try {
+						farthestJet = airField.airCommand.get(0);
+					} catch (Exception e) {
+						System.out.println("An error occurred.\n");
+						break;
+					}
+					if (airField.airCommand != null) {
+						for (int i = 1; i < airField.airCommand.size(); i++) {
+							if (airField.airCommand.get(i).getRange() > farthestJet.getRange()) {
+								farthestJet = airField.airCommand.get(i);
+							}
+						}
+						System.out.println("* Longest range jet: " + farthestJet.getModel() + " @ " + farthestJet.getRange()
+								+ " miles");
+					}
+					System.out.println();
+					break;
+				case "5":
+					System.out.println("\n5. Load all Cargo Jets:\n");
+					for (Jet cargoCarrier : airField.airCommand) {
+						if (cargoCarrier instanceof CargoCarrier) {
+							((CargoCarrier) cargoCarrier).loadCargo();
+						}
+					}
+					System.out.println();
+					break;
+				case "6":
+					System.out.println("\n6. Dogfight!\n");
+					for (Jet jet : airField.airCommand) {
+						if (jet instanceof JetImpl) {
+							jet.fight();
+						}
+					}
+					System.out.println();
+					break;
+				case "7":
+					System.out.println();
+					System.out.println("7. Add a jet to Fleet:\n");
+					String name;
+					int speed;
+					long range;
+					double price;
+					System.out.println("****************");
+					System.out.println("Enter jet Model:");
+					try {
+						name = sc.next();
+					} catch (Exception e) {
+						System.out.println("An error occurred.\n");
+						break;
+					}
+					System.out.println("Enter jet Speed:");
+					try {
+						speed = sc.nextInt();
+					} catch (Exception e) {
+						System.out.println("An error occurred.\n");
+						break;
+					}
+					System.out.println("Enter jet Range:");
+					try {
+						range = sc.nextLong();
+					} catch (Exception e) {
+						System.out.println("An error occurred.\n");
+						break;
+					}
+					System.out.println("Enter jet Price:");
+					try {
+						price = sc.nextDouble();
+					} catch (Exception e) {
+						System.out.println("An error occurred.\n");
+						break;
+					}
+					System.out.println();
+					Jet newJet = new JetImpl(name, speed, range, price);
+					airField.airCommand.add(newJet);
+					System.out.println("Added!");
+					for (Jet jet : airField.airCommand) {
+						System.out.println("* " + jet);
+					}
+					System.out.println();
+					break;
+				case "8":
+					System.out.println("\n8. Remove a jet from Fleet\n");
 
-				System.out.println();
-				break;
-			case "9":
-				System.out.println("\n9. Quit\nQuitting program...\n");
-				System.exit(0);
-			case "help":
-				break;
-			default:
-				System.out.println("\nPlease make a valid selection.\n");
+					System.out.println("Available jets:\n");
+
+					for (int i = 0; i < airField.airCommand.size(); i++) {
+						System.out.println(i + ": " + airField.airCommand.get(i));
+					}
+
+					int toRemove;
+
+					System.out.println("\nWhich jet to remove?\n");
+					try {
+						toRemove = sc.nextInt();
+					} catch (Exception e) {
+						System.out.println("\nUh oh, something happened.\n");
+						System.out.println("\nWhich jet to remove?\n");
+						toRemove = sc.nextInt();
+					}
+
+					try {
+						airField.airCommand.remove(toRemove);
+						System.out.println("Removed.\n");
+						for (Jet jet : airField.airCommand) {
+							System.out.println("* " + jet);
+						}
+					} catch (Exception e) {
+						System.out.println("Hmmm, please try again.");
+					}
+
+					System.out.println();
+					break;
+				case "9":
+					System.out.println("\n9. Quit\nQuitting program...\n");
+					System.exit(0);
+				case "help":
+					break;
+				default:
+					System.out.println("\nPlease make a valid selection.\n");
+				}
+				app.greet();
+				selection = app.getSelection(sc);
 			}
-			app.greet();
-			selection = app.getSelection(sc);
 		}
 	}
 
--- src/com/skilldistillery/jets/entities/AirField.java
+++ src/com/skilldistillery/jets/entities/AirField.java
@@ -1,5 +1,16 @@
 package com.skilldistillery.jets.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AirField {
+	public List<Jet> airCommand = new ArrayList<>();
+
+	public AirField() {
+	}
+
+	public void addAll(List<Jet> jets) {
+		airCommand.addAll(jets);
+	}
 
 }
